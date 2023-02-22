0c1360777a75e7c2eacc042e275285198b0a9da7
Arlo Valiela <arlovaliela@Arlos-MacBook-Pro.local>
2023-02-21 23:36:35 -0500

    almost there

--- js/d3.v6.1.1/main.js
+++ js/d3.v6.1.1/main.js
@@ -1,21 +1,43 @@
 // Homework 5 JS Code
 
-const FRAME_HEIGHT = 400;
-const FRAME_WIDTH = 400;
+const FRAME_HEIGHT = 600;
+const FRAME_WIDTH = 600;
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 
-// with a scale function
+// setting vis dmiensions
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
-const FRAME1 = d3.select('#Scatter')
+
+// functions for event handling
+function pointHoveredOn(event, d) {
+                    d3.select(this).style('fill', 'yellow');
+                }
+
+function pointHoveredOff(event, d) {
+                    d3.select(this)
+                        .style('fill', 'black');
+                }
+                
+function pointClicked(event, d) {
+                    let point = d3.select(this)
+                    point.classed('clicked', !point.node().classList.contains('clicked'));
+                    console.log(d3.select(this));
+                    let newText = 'Last Point Clicked ' + point.id; //not work
+                    document.getElementById('selected_point').innerHTML = newText;
+                    }
+
+// frame for scatter axis
+const FRAME1 = d3.select('.Left')
 				.append('svg')
 					.attr('height', FRAME_HEIGHT)
 					.attr('width', FRAME_HEIGHT)
 					.attr('class', "frame");
 
+// read in scatter data
 d3.csv('data/scatter-data.csv').then((data) => {
 
+    // set max values for scaling
 	const MAX_X = d3.max(data, (d) => {
 								return parseInt(d.x)
 							});
@@ -23,6 +45,7 @@ d3.csv('data/scatter-data.csv').then((data) => {
 								return parseInt(d.y)
 							});
 
+    // scaling functions
 	const xscale = d3.scaleLinear()
             .domain([0, (MAX_X + 1)])
             .range([0, VIS_WIDTH]);
@@ -30,6 +53,8 @@ d3.csv('data/scatter-data.csv').then((data) => {
             .domain([(MAX_Y + 1), 0])
             .range([0, VIS_HEIGHT]);
 
+
+    // plotting points onto frame, assigning attributes
 	FRAME1.selectAll('points')
 			.data(data)
 			.enter()
@@ -45,35 +70,22 @@ d3.csv('data/scatter-data.csv').then((data) => {
                 .attr('id', (d) => {
                     return '(' + d.x + ', ' + d.y + ')'; }); // not work?
 
-
+    // add x axis
 	FRAME1.append('g')
 			.attr('transform', 'translate(' + MARGINS.left + ',' 
 												+ (MARGINS.top + VIS_HEIGHT) +')')
 			.call(d3.axisBottom(xscale))
 				.attr('font-size', '20px');
 
+    // add y axis
     FRAME1.append('g')
             .attr('transform', 'translate(' + MARGINS.left + ',' 
                                                     + MARGINS.top +')')
             .call(d3.axisLeft(yscale))
                     .attr('font-size', '20px');
 
-    function pointHoveredOn(event, d) {
-                        d3.select(this).style('fill', 'yellow');
-                    }
 
-    function pointHoveredOff(event, d) {
-                        d3.select(this)
-                            .style('fill', 'black');
-                    }
-                    
-    function pointClicked(event, d) {
-                        let point = d3.select(this)
-                        point.classed('clicked', !point.node().classList.contains('clicked'));
-                        console.log(d3.select(this));
-                        let newText = 'Last Point Clicked ' + d3.select(this).id; //not work
-                        document.getElementById('selected_point').innerHTML = newText;
-                    }
+
 
     // Add event listeners
     FRAME1.selectAll('.point')
@@ -82,6 +94,39 @@ d3.csv('data/scatter-data.csv').then((data) => {
           .on('click', pointClicked); 
 })
 
+// Add point from user input
+function addUserPoint() {
+    let x = document.getElementById("selectX").value;
+    let y = document.getElementById("selectY").value
+
+    if (x != "X-Coordinate" && y != "Y-Coordinate"){
+
+        console.log(x)
+        console.log(y)
+        const xscale = d3.scaleLinear()
+                .domain([0, 10])
+                .range([0, VIS_WIDTH]);
+        const yscale = d3.scaleLinear()
+                .domain([10, 0])
+                .range([0, VIS_HEIGHT]);
+        console.log(xscale(Number(x)) + MARGINS.left)
+        console.log(yscale(Number(y)) + MARGINS.top)
+        FRAME1.selectAll('.point')
+                .append('circle')
+                            .attr('cx', xscale(Number(x)) + MARGINS.left)
+                            .attr('cy', yscale(Number(y)) + MARGINS.top)
+                            .attr('r', 10)
+                            .attr('fill', "black")
+                            .attr('class', 'point')
+                            .attr('id', (d) => {
+                                return '(' + d.x + ', ' + d.y + ')'; })
+                            .on('mouseover', pointHoveredOn)
+                            .on('mouseout', pointHoveredOff)
+                            .on('click', pointClicked);
+}}
+ 
+// Add button functionality
+document.getElementById("subButton").addEventListener("click", addUserPoint)
 
 
 
@@ -89,27 +134,24 @@ d3.csv('data/scatter-data.csv').then((data) => {
 
 
 
-
-
-
-
-
-
-
-
-const FRAME2 = d3.select('#Barchart')
+// add frame for bar cahrt
+const FRAME2 = d3.select('.Left')
 				.append('svg')
 					.attr('height', FRAME_HEIGHT)
 					.attr('width', FRAME_HEIGHT)
 					.attr('class', "frame");
 
+// read in the bar chart data
 d3.csv('data/bar-data.csv').then((data) => {
 
+
+    // Max values for scaling functions
     const MAX_X = data.length;
     const MAX_Y = d3.max(data, (d) => {
                                 return parseInt(d.amount)
                             });
 
+    // scaling functions
     const xscale = d3.scaleLinear()
             .domain([0, (MAX_X + 1)])
             .range([0, VIS_WIDTH]);
@@ -117,6 +159,7 @@ d3.csv('data/bar-data.csv').then((data) => {
             .domain([(MAX_Y + 1), 0])
             .range([0, VIS_HEIGHT]);
 
+    // adding each bar to chart
     FRAME2.selectAll('bars')
             .data(data)
             .enter()
@@ -131,12 +174,14 @@ d3.csv('data/bar-data.csv').then((data) => {
                 .attr('y', (VIS_HEIGHT + MARGINS.top))
                 .attr('class', 'bar');
 
+    // adding x axis
     FRAME2.append('g')
             .attr('transform', 'translate(' + MARGINS.left + ',' 
                                                 + (MARGINS.top + VIS_HEIGHT) +')')
             .call(d3.axisBottom(xscale))
                 .attr('font-size', '20px');
 
+    // adding y axis
     FRAME2.append('g')
             .attr('transform', 'translate(' + MARGINS.left + ',' 
                                                     + MARGINS.top +')')
